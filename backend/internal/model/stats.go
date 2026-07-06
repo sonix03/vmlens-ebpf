9e221ddf99b5c229e7934b8ae372833228c3b2b4
sonix03 <13523121@std.stei.itb.ac.id>
2026-07-06 10:14:07 +0700

    modified: struct for stats services

--- backend/internal/model/stats.go
+++ backend/internal/model/stats.go
@@ -8,8 +8,14 @@ type Summary struct {
 	StaleVMs        int       `json:"stale_vms"`
 	OfflineVMs      int       `json:"offline_vms"`
 	TotalFlows      int       `json:"total_flows"`
+	InternalFlows   int       `json:"internal_flows"`
+	ExternalFlows   int       `json:"external_flows"`
 	InternalBytes   int64     `json:"internal_bytes"`
+	InternalSent    int64     `json:"internal_sent_bytes"`
+	InternalRecv    int64     `json:"internal_received_bytes"`
 	ExternalBytes   int64     `json:"external_bytes"`
+	ExternalSent    int64     `json:"external_sent_bytes"`
+	ExternalRecv    int64     `json:"external_received_bytes"`
 	UnknownInternal int       `json:"unknown_internal_hosts"`
 	UpdatedAt       time.Time `json:"updated_at"`
 }
