489e9968b0cb739b413e873f906d2607037dbae0
sonix03 <13523121@std.stei.itb.ac.id>
2026-07-09 13:55:01 +0700

    fix: fallback vmlinux header for bpf compile

--- agent/ebpf/vmlinux_fallback.h
+++ agent/ebpf/vmlinux_fallback.h
@@ -1,5 +1,8 @@
 #ifndef __VMLINUX_FALLBACK_H__
 #define __VMLINUX_FALLBACK_H__
+#ifndef __VMLINUX_H__
+#define __VMLINUX_H__
+#endif
 
 typedef signed char __s8;
 typedef unsigned char __u8;
@@ -12,6 +15,41 @@ typedef unsigned long long __u64;
 typedef __u16 __be16;
 typedef __u32 __be32;
 typedef __u32 __wsum;
+
+enum bpf_map_type {
+	BPF_MAP_TYPE_UNSPEC = 0,
+	BPF_MAP_TYPE_HASH = 1,
+	BPF_MAP_TYPE_ARRAY = 2,
+	BPF_MAP_TYPE_PROG_ARRAY = 3,
+	BPF_MAP_TYPE_PERF_EVENT_ARRAY = 4,
+	BPF_MAP_TYPE_PERCPU_HASH = 5,
+	BPF_MAP_TYPE_PERCPU_ARRAY = 6,
+	BPF_MAP_TYPE_STACK_TRACE = 7,
+	BPF_MAP_TYPE_CGROUP_ARRAY = 8,
+	BPF_MAP_TYPE_LRU_HASH = 9,
+	BPF_MAP_TYPE_LRU_PERCPU_HASH = 10,
+	BPF_MAP_TYPE_LPM_TRIE = 11,
+	BPF_MAP_TYPE_ARRAY_OF_MAPS = 12,
+	BPF_MAP_TYPE_HASH_OF_MAPS = 13,
+	BPF_MAP_TYPE_DEVMAP = 14,
+	BPF_MAP_TYPE_SOCKMAP = 15,
+	BPF_MAP_TYPE_CPUMAP = 16,
+	BPF_MAP_TYPE_XSKMAP = 17,
+	BPF_MAP_TYPE_SOCKHASH = 18,
+	BPF_MAP_TYPE_CGROUP_STORAGE = 19,
+	BPF_MAP_TYPE_REUSEPORT_SOCKARRAY = 20,
+	BPF_MAP_TYPE_PERCPU_CGROUP_STORAGE = 21,
+	BPF_MAP_TYPE_QUEUE = 22,
+	BPF_MAP_TYPE_STACK = 23,
+	BPF_MAP_TYPE_SK_STORAGE = 24,
+	BPF_MAP_TYPE_DEVMAP_HASH = 25,
+	BPF_MAP_TYPE_STRUCT_OPS = 26,
+	BPF_MAP_TYPE_RINGBUF = 27,
+};
+
+enum {
+	BPF_ANY = 0,
+};
 
 struct in6_addr {
 	union {
@@ -41,6 +79,60 @@ struct sock_common {
 
 struct sock {
 	struct sock_common __sk_common;
+} __attribute__((preserve_access_index));
+
+struct pt_regs {
+	unsigned long r15;
+	unsigned long r14;
+	unsigned long r13;
+	unsigned long r12;
+	union {
+		unsigned long bp;
+		unsigned long rbp;
+	};
+	union {
+		unsigned long bx;
+		unsigned long rbx;
+	};
+	unsigned long r11;
+	unsigned long r10;
+	unsigned long r9;
+	unsigned long r8;
+	union {
+		unsigned long ax;
+		unsigned long rax;
+	};
+	union {
+		unsigned long cx;
+		unsigned long rcx;
+	};
+	union {
+		unsigned long dx;
+		unsigned long rdx;
+	};
+	union {
+		unsigned long si;
+		unsigned long rsi;
+	};
+	union {
+		unsigned long di;
+		unsigned long rdi;
+	};
+	union {
+		unsigned long orig_ax;
+		unsigned long orig_rax;
+	};
+	union {
+		unsigned long ip;
+		unsigned long rip;
+	};
+	unsigned long cs;
+	unsigned long flags;
+	union {
+		unsigned long sp;
+		unsigned long rsp;
+	};
+	unsigned long ss;
 } __attribute__((preserve_access_index));
 
 #endif
